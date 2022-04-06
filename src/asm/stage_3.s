be6eb2d513e2580106f7733ade0cc0176ba897aa
Hugo van de Kuilen <hugo.vandekuilen1234567890@gmail.com>
2022-04-06 13:47:44 +0200

    comments

--- src/asm/stage_3.s
+++ src/asm/stage_3.s
@@ -152,16 +152,16 @@ check_and_enable_sse:
     # Check for SSE support (accelerated floating point math, software math is incredibly slow)
     mov eax, 0x1
     cpuid
-    test edx, (1 << 25)
-    jz no_sse
+    test edx, (1 << 25) # test sets ZF(zero_flag) to the specified bit (1<<25) in edx
+    jz no_sse           # Jump to error handler if ZF == 0 (jz = Jump if Zero)
 
     # If SSE is supported, enable it
     mov eax, cr0
-    and ax, 0xFFFB      # clear coprocessor emulation CR0.EM
-    or ax, 0x2          # set coprocessor monitoring  CR0.MP
+    and ax, 0xFFFB      # Clear coprocessor emulation CR0.EM
+    or ax, 0x2          # Set coprocessor monitoring  CR0.MP
     mov cr0, eax
     mov eax, cr4
-    or ax, (3 << 9)     # set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    or ax, (3 << 9)     # Set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
     mov cr4, eax
     ret
 no_sse:
